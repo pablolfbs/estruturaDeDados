c11dbdb40f34078d1258b65965f0e91e15f99817
ads.2n <ads.2n@L606-16.senacrjedu.local>
2017-04-27 21:58:18 -0300

    fsad

--- Search/src/test/SearchTest.java
+++ Search/src/test/SearchTest.java
@@ -1,8 +1,9 @@
 package test;
 
 import java.util.Random;
-
+import search.BinarySearch;
 import search.LinearSearch;
+import sort.BubbleSort;
 
 public class SearchTest {
 	
@@ -22,7 +23,21 @@ public class SearchTest {
 			System.out.print(array[i] + " ");
 		}
 		System.out.println();
-		System.out.println(search.linearSearch(array, 12));
+//		System.out.println(search.linearSearch(array, 12));
+		
+		BubbleSort sort = new BubbleSort();
+		array = sort.bubbleSort(array);
+		
+		System.out.println();
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		
+		System.out.println();
+		
+		BinarySearch search1 = new BinarySearch();
+		System.out.println(search1.binarySearch(array, 12));
 		
 	}
 
