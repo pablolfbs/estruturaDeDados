193509c5bf6c2d6bb1ae06dd0e3f3ef6678780fd
pablo <pablo@DESKTOP-FM56SOR.home>
2017-06-21 17:40:53 -0300

    fsdsds

---
+++ HanoiTowerInterativo/src/stack/LinkedStack.java
@@ -0,0 +1,75 @@
+package stack;
+
+import java.util.NoSuchElementException;
+
+public class LinkedStack {
+	
+	private Node source;
+	private Node destination;
+	private Node aux;
+	private int size;
+	
+	public void executeHanoi() {
+		source.setNext(destination);
+	}
+	public LinkedStack() {
+		this.top = null;
+		this.size = 0;
+	}
+	
+	public int size() {
+		return this.size;
+	}
+	
+	public boolean isEmpty() {
+		return (this.top) == null;
+	}
+	
+	public void push(int content) {
+		Node oldTop = top;
+		
+//		Node newNode = new Node(content);
+//		top = newNode;
+		
+		top = new Node(content);
+		top.setNext(oldTop);
+		
+		size++;
+	}
+	
+	//Desempilhamento
+	public int pop() {
+		
+		if(isEmpty()) {
+			throw new NoSuchElementException("Stack is empty"); 
+		}
+		Node oldTop = top;
+		int content = oldTop.getContent();
+		top = oldTop.getNext();
+		oldTop.setNext(null);
+		
+		size--;
+		
+		return content;
+	}
+	
+	//Olhadinha ou Espiada: não desempilha
+	public int peek() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("Stack is empty");
+		}
+		return top.getContent();
+	}
+	
+	public String toString() {
+		String str = "";
+		
+		Node tmp = top;
+		
+		for (int i = 0; i < this.size; i++) {
+			str += tmp.getContent() + "\n";
+			tmp = tmp.getNext();
+		}
+		return str;
+	}
+}
