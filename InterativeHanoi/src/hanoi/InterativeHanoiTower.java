b775f6d92e9d33400e69676699d464a9803885b4
ads.2n <ads.2n@L606-16.senacrjedu.local>
2017-06-22 20:07:30 -0300

    asdfs

--- InterativeHanoi/src/hanoi/InterativeHanoiTower.java
+++ InterativeHanoi/src/hanoi/InterativeHanoiTower.java
@@ -3,17 +3,31 @@ package hanoi;
 import stack.LinkedStack;
 
 public class InterativeHanoiTower {
-	
+
+	//Responsável pelo empilhamento dos comandos
 	private LinkedStack stack = new LinkedStack();
-	
+
+	// Realiza o movimento
 	private void move(int source, int destination) {
 		System.out.println(source + " -> " + destination);
 	}
-	
+
+	// Método interativo da Torre de Hanoi
 	public void executeHanoi(int n, int source, int destination, int aux) {
-		while(n > 1) {
-			
-			n--;
+
+		// Declarando e empilhando o primeiro comando
+		Command currentCommand = new Command(n, source, destination, aux);
+		stack.push(currentCommand);
+
+		// Executamos o restante do alg enquanto tivermos comandos da pilha
+		while (!stack.isEmpty()) {
+			// Empilha um novo comando sempre que n > 1
+			if (n > 1) {
+				n--;
+				//Seria uma chamada recursiva
+				currentCommand = new Command(n, source, aux, destination);
+				stack.push(currentCommand);
+			}
 		}
 	}
 }
